6facd5492d40eb994d55856c402b703d814540f8
ombuna <ombunaw@gmail.com>
2022-06-01 17:50:37 +0300

    firts commit

---
+++ BethanysPieShop/BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories => new List<Category>
+        { 
+        new Category {CategoryTd = 1 ,CategoryName ="Frut Pies",Description="All Fruit Pies"},
+         new Category {CategoryTd = 2 ,CategoryName ="Cheese Cakes",Description="Cheesy All The way"},
+         new Category {CategoryTd = 3 ,CategoryName ="Seasonal pies",Description="Get In The Mood Of A Season Pie"}
+        };
+    }
+}
---
+++ BethanysPieShop/BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        public IEnumerable<Pie> AllPies => new List<Pie>
+        {
+             new Pie {PieId=1,Name="Strawberry Pie",Price=15.95M,ShortDescription="hello"},
+             new Pie {PieId=1,Name="Cheese cake",Price=18.95M,ShortDescription="hello"},
+             new Pie {PieId=1,Name="Rhubarb Pie",Price=15.95M,ShortDescription="hello"},
+             new Pie {PieId=1,Name="Pumkin Pie",Price=12.95M,ShortDescription="hello"}
+        };
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
